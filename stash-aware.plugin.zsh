9b69ee6b650b519c225ffa82fc1b9923ec48c571
Marvin Frommhold <depressiveRobot@users.noreply.github.com>
2015-05-07 21:55:59 +0200

    fix sourcing of function

--- stash-aware.plugin.zsh
+++ stash-aware.plugin.zsh
@@ -1,5 +1,16 @@
 #!/bin/zsh
 
-source stash-aware.sh
+function _show_branch_stashes {
+  branchName=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+  if [[ "$branchName" != "" ]]
+  then
+    if [[ "$(git stash list 2>/dev/null | grep $branchName)" != "" ]]
+    then
+      echo
+      echo -e "\e[1;33mStashes for current branch:\e[0m"
+      git stash list 2>/dev/null | grep -i --color=never "On $branchName"
+    fi
+  fi
+}
 
-add-zsh-hook precmd show_branch_stashes
+add-zsh-hook precmd _show_branch_stashes
